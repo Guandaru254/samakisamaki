37dd4c2d7cc663cf9e4fb3bee796698aa96fd447
Guandaru254 <leroykim82@gmail.com>
2025-02-17 01:36:37 +0300

    Updated migrations, models, and deploy workflow

--- menu/models.py
+++ menu/models.py
@@ -1,8 +1,7 @@
-# menu/models.py
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -12,15 +11,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='menu_items/')
-    category = models.CharField(max_length=100, choices=(
-        ('starters', 'Starters'),
-        ('main_courses', 'Main Courses'),
-        ('desserts', 'Desserts'),
-        ('beverages', 'Beverages'),
-        ('seafood', 'Seafood'),
-        ('specials', 'Specials'),
-    ))
+    image = models.ImageField(upload_to='menu_items/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Fixed ForeignKey issue
     vegetarian = models.BooleanField(default=False)
     gluten_free = models.BooleanField(default=False)
 
--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, unique=True, blank=False, null=False)  # Updated
+    phone_number = models.CharField(max_length=15, unique=True, blank=False, null=False, default="0700000000") # Updated
     address = models.TextField(blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
